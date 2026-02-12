60360b9bdb5dea698934967ab2e1e364c8b203b4
calebjames02 <calebjamesbergen@gmail.com>
2026-02-11 18:18:21 -0800

    finished time to collision function and cleaned up code

--- testing.py
+++ testing.py
@@ -8,51 +8,48 @@ class Vehicle:
     x_vel: float
     y_vel: float
 
+    """
+    Purpose: Return whether or not the calling vehicle is in front of the 'other' vehicle
+                Used in closest_same_lane function
+    """
+    def is_ahead_of(self, other):
+        return self.x_pos > other.x_pos and self.lane == other.lane
+
+    """
+    Purpose: Return how many seconds it will take for the calling vehicle to crash into the given vehicle
+                Return infinity if no collision will ever occur given current positions and speeds
+    """
+    def get_ttc(self, lead_car):
+        if not lead_car: return float('inf')
+
+        rel_dst = lead_car.x_pos - self.x_pos
+        rel_vel = self.x_vel - lead_car.x_vel
+
+        if rel_dst <= 0: return 0.0
+        if rel_vel <= 0: return float('inf')
+
+        return rel_dst / rel_vel
+
 """
 Purpose: From the cars list find the closest vehicle in front of the ego vehicle, if one exists
 """
 def closest_same_lane(cars):
-    if not cars:
-        return "No cars currently exist"
+    if len(cars) <= 1:
+        return None
 
-    if len(cars) == 1:
-        return "Only the ego vehicle exists"
-
-    # Mark down ego lane and position for future access
-    ego_lane = cars[0].lane
-    ego_pos = cars[0].x_pos
-
-    # Filter vehicles out of lane_cars list that aren't in the same lane as the ego vehicle
-    lane_cars = [car for car in cars if car.lane == ego_lane]
-
-    if(len(lane_cars) == 1):
-        return f"There is no car close to the ego vehicle in lane {ego_lane}"
-
+    ego = cars[0]
     closest_car = None
     min_dist = float('inf')
-        
-    for car in lane_cars[1:]:
-        dist = car.x_pos - ego_pos
-        if dist > 0 and dist < min_dist:
-            min_dist = dist
-            closest_car = car
 
-    if min_dist == float('inf'):
-        return f"There is no car close to the ego vehicle in lane {ego_lane}"
+    # Check all cars in cars list to see which is closest to ego vehicle that is in the same lane
+    for car in cars[1:]:
+        if car.is_ahead_of(ego):
+            dist = car.x_pos - ego.x_pos
+            if 0 < dist < min_dist:
+                min_dist = dist
+                closest_car = car
 
-    return f"The closest vehicle to the ego vehicle in lane {ego_lane} is {closest_car.name} at position x = {closest_car.x_pos}"
-
-def time_to_collision(ego, car):
-    relative_dist = car.x_pos - ego.x_pos
-    relative_speed = ego.x_vel - car.x_vel
-
-    if relative_dist <= 0:
-        return 0.0
-
-    if relative_speed <= 0:
-        return float('inf')
-
-    return relative_dist / relative_speed
+    return closest_car
 
 def run_test(test_name, correct_statement, check_statement):
     print(f"Test {test_name}:", end=" ")
@@ -69,26 +66,25 @@ def run_test(test_name, correct_statement, check_statement):
 empty_lane = [Vehicle(name="Ego vehicle", x_pos=0, lane=1, x_vel=20, y_vel=0),
               Vehicle(name="Vehicle 1", x_pos=25, lane=2, x_vel=20, y_vel=0),
               Vehicle(name="Vehicle 2", x_pos=25, lane=3, x_vel=20, y_vel=0)]
-run_test("NO CARS IN SAME LANE 1", "There is no car close to the ego vehicle in lane 1", closest_same_lane(empty_lane))
+run_test("NO CARS IN SAME LANE 1", None, closest_same_lane(empty_lane))
 print()
 
 error1 = []
 error2 = [Vehicle(name="Ego vehicle", x_pos=0, lane=1, x_vel=20, y_vel=0)]
-run_test("Error 1", "No cars currently exist", closest_same_lane(error1))
-run_test("Error 2", "Only the ego vehicle exists", closest_same_lane(error2))
+run_test("Error 1", None, closest_same_lane(error1))
+run_test("Error 2", None, closest_same_lane(error2))
 print()
 
-
-cars1 = [Vehicle(name="Ego vehicle", x_pos=25, lane=1, x_vel=20, y_vel=0),
+cars1 = [Vehicle(name="Ego vehicle", x_pos=25, lane=1, x_vel=25, y_vel=0),
          Vehicle(name="Vehicle 1", x_pos=0, lane=1, x_vel=20, y_vel=0),
          Vehicle(name="Vehicle 2", x_pos=100, lane=1, x_vel=20, y_vel=0)]
-cars2 = [Vehicle(name="Ego vehicle", x_pos=0, lane=1, x_vel=20, y_vel=0),
+cars2 = [Vehicle(name="Ego vehicle", x_pos=0, lane=1, x_vel=24, y_vel=0),
         Vehicle(name="Vehicle 1", x_pos=25, lane=1, x_vel=20, y_vel=0),
         Vehicle(name="Vehicle 2", x_pos=100, lane=1, x_vel=20, y_vel=0)]
-cars3 = [Vehicle(name="Ego vehicle", x_pos=0, lane=1, x_vel=20, y_vel=0),
+cars3 = [Vehicle(name="Ego vehicle", x_pos=0, lane=1, x_vel=21, y_vel=0),
          Vehicle(name="Vehicle 1", x_pos=25, lane=1, x_vel=20, y_vel=0),
          Vehicle(name="Vehicle 2", x_pos=10, lane=1, x_vel=20, y_vel=0)]
-cars4 = [Vehicle(name="Ego vehicle", x_pos=0, lane=1, x_vel=20, y_vel=0),
+cars4 = [Vehicle(name="Ego vehicle", x_pos=0, lane=1, x_vel=40, y_vel=0),
          Vehicle(name="Vehicle 1", x_pos=25, lane=1, x_vel=20, y_vel=0),
          Vehicle(name="Vehicle 2", x_pos=25, lane=1, x_vel=20, y_vel=0)]
 cars5 = [Vehicle(name="Ego vehicle", x_pos=25, lane=1, x_vel=20, y_vel=0),
@@ -99,12 +95,21 @@ cars6 = [Vehicle(name="Ego vehicle", x_pos=50, lane=1, x_vel=20, y_vel=0),
         Vehicle(name="Vehicle 2", x_pos=0, lane=1, x_vel=20, y_vel=0)]
 cars7 = [Vehicle(name="Ego vehicle", x_pos=50, lane=1, x_vel=20, y_vel=0),
          Vehicle(name="Vehicle 1", x_pos=-25, lane=1, x_vel=20, y_vel=0),
-         Vehicle(name="Vehicle 2", x_pos=75, lane=1, x_vel=20, y_vel=0)]
+         Vehicle(name="Vehicle 2", x_pos=75, lane=1, x_vel=22, y_vel=0)]
 
-run_test("CLOSEST 1", "The closest vehicle to the ego vehicle in lane 1 is Vehicle 2 at position x = 100", closest_same_lane(cars1))
-run_test("CLOSEST 2", "The closest vehicle to the ego vehicle in lane 1 is Vehicle 1 at position x = 25", closest_same_lane(cars2))
-run_test("CLOSEST 3", "The closest vehicle to the ego vehicle in lane 1 is Vehicle 2 at position x = 10", closest_same_lane(cars3))
-run_test("CLOSEST 4", "The closest vehicle to the ego vehicle in lane 1 is Vehicle 1 at position x = 25", closest_same_lane(cars4))
-run_test("CLOSEST 5", "There is no car close to the ego vehicle in lane 1", closest_same_lane(cars5))
-run_test("CLOSEST 6", "There is no car close to the ego vehicle in lane 1", closest_same_lane(cars6))
-run_test("CLOSEST 7", "The closest vehicle to the ego vehicle in lane 1 is Vehicle 2 at position x = 75", closest_same_lane(cars7))
+run_test("CLOSEST 1", Vehicle(name="Vehicle 2", x_pos=100, lane=1, x_vel=20, y_vel=0), closest_same_lane(cars1))
+run_test("CLOSEST 2", Vehicle(name="Vehicle 1", x_pos=25, lane=1, x_vel=20, y_vel=0), closest_same_lane(cars2))
+run_test("CLOSEST 3", Vehicle(name="Vehicle 2", x_pos=10, lane=1, x_vel=20, y_vel=0), closest_same_lane(cars3))
+run_test("CLOSEST 4", Vehicle(name="Vehicle 1", x_pos=25, lane=1, x_vel=20, y_vel=0), closest_same_lane(cars4))
+run_test("CLOSEST 5", None, closest_same_lane(cars5))
+run_test("CLOSEST 6", None, closest_same_lane(cars6))
+run_test("CLOSEST 7", Vehicle(name="Vehicle 2", x_pos=75, lane=1, x_vel=22, y_vel=0), closest_same_lane(cars7))
+print()
+
+run_test("TIME 1", 15, cars1[0].get_ttc(closest_same_lane(cars1)))
+run_test("TIME 2", 6.25, cars2[0].get_ttc(closest_same_lane(cars2)))
+run_test("TIME 3", 10, cars3[0].get_ttc(closest_same_lane(cars3)))
+run_test("TIME 4", 1.25, cars4[0].get_ttc(closest_same_lane(cars4)))
+run_test("TIME 5", float('inf'), cars5[0].get_ttc(closest_same_lane(cars5)))
+run_test("TIME 6", float('inf'), cars6[0].get_ttc(closest_same_lane(cars6)))
+run_test("TIME 7", float('inf'), cars7[0].get_ttc(closest_same_lane(cars7)))
